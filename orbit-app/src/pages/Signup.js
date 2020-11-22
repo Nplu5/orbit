f9bff3685d26d634dafbb4347baeb883622837f2
Simon <skrause@fzi.de>
2020-11-22 19:13:51 +0100

    Implement signup page to working example

--- orbit-app/src/pages/Signup.js
+++ orbit-app/src/pages/Signup.js
@@ -10,6 +10,8 @@ import GradientBar from './../components/common/GradientBar';
 import FormError from './../components/FormError';
 import FormSuccess from './../components/FormSuccess';
 import logo from './../images/logo.png';
+import { publicFetch } from "./../util/fetch"
+import { Redirect } from 'react-router';
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string().required(
@@ -27,9 +29,17 @@ const Signup = () => {
   const [signupError, setSignupError] = useState();
   const [loginLoading, setLoginLoading] = useState(false);
 
+  const [redirectOnLogin, setRedirectOnLogin] = useState(false)
+
   const submitCredentials = async credentials => {
     try {
       setLoginLoading(true);
+      const { data } = await publicFetch.post('signup', credentials)
+      setSignupSuccess(data.message)
+      setSignupError('')
+      setTimeout(() => {
+        setRedirectOnLogin(true)
+      }, 700)
     } catch (error) {
       setLoginLoading(false);
       const { data } = error.response;
@@ -40,6 +50,7 @@ const Signup = () => {
 
   return (
     <>
+      {redirectOnLogin && <Redirect to="/dashboard" />}
       <section className="w-1/2 h-screen m-auto p-8 sm:pt-10">
         <GradientBar />
         <Card>
