d8edda91980be713fa5df4ec043f8e1d4e53ddbf
Simon <skrause@fzi.de>
2020-11-23 21:23:51 +0100

    Add AuthState to local storage (bad practice)

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -1,17 +1,50 @@
-import React, { createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
+  
+  const [authState, setAuthState] = useState({
+    token: null,
+    expiresAt: null,
+    userInfo: {}
+  })
+  
+  useEffect(() => {    
+    const token = localStorage.getItem("token")
+    const userInfo = localStorage.getItem("userInfo")
+    const expiresAt = localStorage.getItem("expiresAt")
+    setAuthState({
+      token,
+      expiresAt,
+      userInfo: userInfo ? JSON.parse(userInfo) : {}
+    })
+  }, [])
+
+  const setAuthInfo = ({token, userInfo, expiresAt}) => {
+    localStorage.setItem("token", token)
+    localStorage.setItem("userInfo", JSON.stringify(userInfo))
+    localStorage.setItem("expiresAt", expiresAt)
+    setAuthState({
+      token, 
+      userInfo,
+      expiresAt
+    })
+  }
+  const isAuthenticated = () => {
+    if(!authState.token && !authState.expiresAt){
+      return false
+    }
+    return new Date().getTime() / 1000 < authState.expiresAt
+  }
+
   return (
     <Provider
       value={{
-        authState: {
-          token: null,
-          expiresAt: null,
-          userInfo: {}
-        }
+        authState,
+        setAuthState: authInfo => setAuthInfo(authInfo),
+        isAuthenticated
       }}
     >
       {children}
