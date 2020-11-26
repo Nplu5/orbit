186ff542adcc6c8c6e66cce7158dc3e8498b6c58
Simon <skrause@fzi.de>
2020-11-26 16:36:56 +0100

    Set up proxy

--- orbit-app/src/context/FetchContext.js
+++ orbit-app/src/context/FetchContext.js
@@ -13,12 +13,8 @@ const FetchProvider = ({ children }) => {
   });
 
   authAxios.interceptors.request.use(
-    config => {
-      const { origin } = new URL(config.baseURL)
-      const allowedOrigins = ['http://localhost:3001']
-      if(allowedOrigins.includes(origin)){
-        config.headers.Authorization = `Bearer ${authContext.authState.token}`
-      } 
+    config => {      
+      config.headers.Authorization = `Bearer ${authContext.authState.token}`
       return config
     },
     error => {
